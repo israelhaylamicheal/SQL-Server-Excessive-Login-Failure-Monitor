0d5c7a502bd9b41252a8c28827b2c00e2c4c73e7
@Israel <31446865+israelhaylamicheal@users.noreply.github.com>
2025-01-26 22:56:04 -0500

    Update and rename DetectExcessiveLogonFailure.sql to DetectExcessiveLoginFailure.sql

--- DetectExcessiveLoginFailure.sql
+++ DetectExcessiveLoginFailure.sql
@@ -1,10 +1,10 @@
--- Script to monitor excessive failed logon attempts and send an alert
+-- Script to monitor excessive failed Login attempts and send an alert
 USE [master];
 GO
 
-CREATE OR ALTER PROCEDURE [dbo].[usp_MonitorExcessiveLogonFailures] 
+CREATE OR ALTER PROCEDURE [dbo].[usp_MonitorExcessiveLoginFailures] 
 (
-    @Duration INT,              -- Time window in minutes to check for failed logon attempts
+    @Duration INT,              -- Time window in minutes to check for failed Login attempts
     @AlertThreshold INT         -- Threshold for sending an alert
 )
 AS
@@ -15,8 +15,8 @@ BEGIN
     DECLARE @StartTime DATETIME = DATEADD(MINUTE, -@Duration, GETDATE());
     DECLARE @EndTime DATETIME = GETDATE();
     DECLARE @Html NVARCHAR(MAX) = '';
-    DECLARE @Subject NVARCHAR(MAX) = 'Alert: Multiple Failed Logon Attempts Detected on ' + @@SERVERNAME;
-    DECLARE @FailedLogonCount INT;
+    DECLARE @Subject NVARCHAR(MAX) = 'Alert: Multiple Failed Login Attempts Detected on ' + @@SERVERNAME;
+    DECLARE @FailedLoginCount INT;
 
     -- Temporary table for error log data
     IF OBJECT_ID('tempdb..#ErrorLog') IS NOT NULL
@@ -28,17 +28,17 @@ BEGIN
         [Text] NVARCHAR(MAX)
     );
 
-    -- Populate the temporary table with failed logon attempts from the SQL Server error log
+    -- Populate the temporary table with failed login attempts from the SQL Server error log
     INSERT INTO #ErrorLog (LogDate, ProcessInfo, [Text])
     EXEC xp_readerrorlog 0, 1, N'Login failed for user', NULL, NULL, NULL, 'DESC';
 
-    -- Count the number of failed logon attempts in the specified time window
-    SELECT @FailedLogonCount = COUNT(*)
+    -- Count the number of failed login attempts in the specified time window
+    SELECT @FailedLoginCount = COUNT(*)
     FROM #ErrorLog
     WHERE LogDate BETWEEN @StartTime AND @EndTime;
 
-    -- Check if the failed logon attempts exceed the alert threshold
-    IF (@FailedLogonCount >= @AlertThreshold)
+    -- Check if the failed login attempts exceed the alert threshold
+    IF (@FailedLoginCount >= @AlertThreshold)
     BEGIN
         -- Generate the HTML table for the email body
         SELECT @Html += '<tr>'
@@ -53,17 +53,17 @@ BEGIN
 
         -- Create the full HTML body
         SET @Html = 
-            N'<h4 style="font-weight: bold; color: red;">Multiple Failed Logon Attempts Detected on ' + @@SERVERNAME + '.</h4>' + 
+            N'<h4 style="font-weight: bold; color: red;">Multiple Failed Login Attempts Detected on ' + @@SERVERNAME + '.</h4>' + 
             '<table border="1" style="padding: 5px; color: black; font-family: Segoe UI; text-align: left; border-collapse: collapse; width: 100%;">
                 <tr style="font-size: 12px; font-weight: normal; background: #BABABB;">
-                    <th>Failed Logon Count</th>
-                    <th>Latest Logon Date</th>
+                    <th>Failed Login Count</th>
+                    <th>Latest Login Date</th>
                     <th>Message</th>
                 </tr>' + @Html + 
             '</table>' +
             N'<p><b>Server:</b> ' + @@SERVERNAME + N'<br>
-            <b>Total Logon Failure Count:</b> ' + CAST(@FailedLogonCount AS NVARCHAR(55)) + N'<br>
-            <b>Job Name:</b> DBA - Eagle Eye Excessive Logon Failure Monitor<br>
+            <b>Total Login Failure Count:</b> ' + CAST(@FailedLoginCount AS NVARCHAR(55)) + N'<br>
+            <b>Job Name:</b> DBA - Eagle Eye Excessive Login Failure Monitor<br>
             <b>Reporting Date:</b> ' + CONVERT(NVARCHAR(55), GETDATE(), 120) + N'</p>';
 
         -- Send the email alert
